00310fb41105ee1586d4fdcd844fcb1b27a0e1af
Weihao <liu3hao@yahoo.com.sg>
2017-10-27 13:51:41 +0800

    added url online

--- src/common/mainConfig.ts
+++ src/common/mainConfig.ts
@@ -1,3 +1,4 @@
 export class MainConfig {
-  public static baseUrl: string = "http://127.0.0.1:8000/"
+  // public static baseUrl: string = "http://127.0.0.1:8000/"
+  public static baseUrl: string = "http://ec2-54-169-64-180.ap-southeast-1.compute.amazonaws.com:8000/"
 }
